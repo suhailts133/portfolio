0ab31eecf742e128d11371e52645a921d04ff05f
Muhammed Suhail TS <126490542+suhailts133@users.noreply.github.com>
2024-10-13 19:51:57 +0530

    message submission

--- main.js
+++ main.js
@@ -1,2 +1,9 @@
-const tooltips = document.querySelectorAll(".tt")
-tooltips.forEach(t => new bootstrap.Tooltip(t))
+function sendMail(){
+    let params = {
+        name:document.getElementById("name").value,
+        email:document.getElementById("email").value,
+        subject:document.getElementById("subject").value,
+        message:document.getElementById("message").value,
+    }
+    emailjs.send("service_oo4xg2d","template_nyy0p64", params).then((alert("your email has been send")))
+}
